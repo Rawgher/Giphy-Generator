863184db3ef0efd400020ed3d643bd7a744d4ebe
Roger <rdpender3@gmail.com>
2018-07-10 16:36:22 -0700

    working on html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,11 +11,28 @@ $("button").on("click", function() {
         url: queryURL,
         method: "GET"
       })
-
+      // what responses looking for (still image, rating, )
       // functions here
 
+      // for loop for making it happen 10 times
+      for (var i = 0; i < results.length; i++) { // referred to response as results here, need to make a variable if going to do this
+        var superHeroDiv = $("<div class='fl w-20'>");
+
+        var p = $("<p>").text("Rating: " + results[i].rating); // once again, need to have results variable above to make this work
+
+        var superHeroImage = $("<img>");
+
+        superHeroImage.attr("src", );//need results still image reference here
+
+        superHeroDiv.append(superHeroImage, p);
+
+        $("#gifZone").html(superHeroDiv);
+
+      }
+      // need function for playing and pausing 
+
       // for loop to create elements for each response
-      // need to prepend at the end
+      // need to have content overwrite previous stuff
 
 
 
