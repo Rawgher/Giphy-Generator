3b29abe182b6f64cee8191200243289ce39cf473
Roger <rdpender3@gmail.com>
2018-07-10 16:19:06 -0700

    added some js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,26 @@
 var apiKey = "ii1cKp76UP9kphOInoWXkQHcjt0F5BGg";
 
-var URL = "http://api.giphy.com/v1/gifs/search?q=" + _____ + "&api_key=" + apiKey + "&limit=10"
+
+$("button").on("click", function() {
+
+    var superHero = $(this).attr("data-hero");
+
+    var URL = "http://api.giphy.com/v1/gifs/search?q=" + superHero + "&api_key=" + apiKey + "&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      // functions here
+
+      // for loop to create elements for each response
+      // need to prepend at the end
+
+
+
+
+
+
+
+});
