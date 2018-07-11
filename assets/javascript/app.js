fe346e008968620ef72059b5ae79de09d406f6a5
Roger <rdpender3@gmail.com>
2018-07-10 17:11:56 -0700

    got submit to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,60 @@
-var apiKey = "ii1cKp76UP9kphOInoWXkQHcjt0F5BGg";
+
+var superHeros = ["Deadpool", "Superman", "Spiderman", "Dr. Strange"];
+
+// $("button").on("click", function() {
+
+//     var superHero = $(this).attr("data-hero");
+//     var apiKey = "ii1cKp76UP9kphOInoWXkQHcjt0F5BGg";
+
+//     var URL = "http://api.giphy.com/v1/gifs/search?q=" + superHero + "&api_key=" + apiKey + "&limit=10";
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//       }).then(function(response) {
+//           //grab response stuff here
+//           // what responses looking for (still image, rating, )
+//       })
+      
+//       // functions here
+function createButtons() {
+    
+    $("#buttonZone").empty();
+
+    for (var i = 0; i < superHeros.length; i++) {
+        var button = $("<a class='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-purple' href='#0'></a>");
+        button.attr("data-name", superHeros[i]);
+        button.text(superHeros[i]);
+        $("#buttonZone").html(button);
+
+    }
+}
+
+$("#superHeroName").on("click", function(event){
+    event.preventDefault();
+    var superHero = $("#superHeroInput").val().trim();
+    superHeros.push(superHero);
+// find a way to get the text to disappear in input
+
+    createButtons();
+})
 
 
-$("button").on("click", function() {
+    //   // for loop for making it happen 10 times
+    //   for (var i = 0; i < results.length; i++) { // referred to response as results here, need to make a variable if going to do this
+    //     var superHeroDiv = $("<div class='fl w-20'>");
 
-    var superHero = $(this).attr("data-hero");
+    //     var p = $("<p>").text("Rating: " + results[i].rating); // once again, need to have results variable above to make this work
 
-    var URL = "http://api.giphy.com/v1/gifs/search?q=" + superHero + "&api_key=" + apiKey + "&limit=10";
+    //     var superHeroImage = $("<img>");
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      // what responses looking for (still image, rating, )
-      // functions here
+    //     superHeroImage.attr("src", );//need results still image reference here
 
-      // for loop for making it happen 10 times
-      for (var i = 0; i < results.length; i++) { // referred to response as results here, need to make a variable if going to do this
-        var superHeroDiv = $("<div class='fl w-20'>");
+    //     superHeroDiv.append(superHeroImage, p);
 
-        var p = $("<p>").text("Rating: " + results[i].rating); // once again, need to have results variable above to make this work
+    //     $("#gifZone").html(superHeroDiv);
 
-        var superHeroImage = $("<img>");
-
-        superHeroImage.attr("src", );//need results still image reference here
-
-        superHeroDiv.append(superHeroImage, p);
-
-        $("#gifZone").html(superHeroDiv);
-
-      }
+    //   }
       // need function for playing and pausing 
 
       // for loop to create elements for each response
@@ -40,4 +66,4 @@ $("button").on("click", function() {
 
 
 
-});
+// });
