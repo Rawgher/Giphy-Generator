d7703d2aac86ee0f22fc6385b01cc1b925651ea7
Roger <rdpender3@gmail.com>
2018-07-10 18:57:15 -0700

    added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,11 @@
 
 var superHeros = ["Deadpool", "Superman", "Spiderman", "Dr. Strange"];
-
-$("button").on("click", function() {
+// click function isnt working
+$("#button").on("click", function() {
 
     var superHero = $(this).attr("data-hero");
+    console.log(this , "what is this on click");
+
     var apiKey = "ii1cKp76UP9kphOInoWXkQHcjt0F5BGg";
 
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + superHero + "&api_key=" + apiKey + "&limit=10";
@@ -26,7 +28,7 @@ $("button").on("click", function() {
 
     var superHeroImage = $("<img>");
 
-    superHeroImage.attr("src", );//need results still image reference here
+    superHeroImage.attr("src", results[i].images.original_still.url);//need results still image reference here
 
     superHeroDiv.append(superHeroImage, p);
 
@@ -48,7 +50,7 @@ function createButtons() {
     $("#buttonZone").empty();
 
     for (var i = 0; i < superHeros.length; i++) {
-        var button = $("<a class='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-purple' href='#0'></a>");
+        var button = $("<a class='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-purple' href='#0' id='button'></a>");
         button.attr("data-hero", superHeros[i]);
         button.text(superHeros[i]);
         $("#buttonZone").append(button);
@@ -58,8 +60,8 @@ function createButtons() {
 
 $("#superHeroName").on("click", function(event){
     event.preventDefault();
-    var superHero = $("#superHeroInput").val().trim();
-    superHeros.push(superHero);
+    var supers = $("#superHeroInput").val().trim();
+    superHeros.push(supers);
 // find a way to get the text to disappear in input
 
     createButtons();
